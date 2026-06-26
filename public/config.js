adeed9d75dd164052023728400dc0e361561f095
Yangcheng Wu <112789642+Scottwyc@users.noreply.github.com>
2026-06-27 01:20:57 +0800

    Update Apps Script URL and email guidance

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.UPLOAD_PORTAL_CONFIG = {
   // Replace this with the deployed Google Apps Script Web App URL.
   // Example: https://script.google.com/macros/s/AKfycbx.../exec
-  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbxiz_N_y3gXPnbi2l98tdNQZioee4eNLC0_LSSivlHUT93Y0O2tQTk4ZVFSNcIqeAoN/exec'
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbyVAcJZKv-wesuIqbSS_YTjBOpj93EpdPWH6rBli61d71wCw0da1Myl5ahgCX6TBo9R/exec'
 };
