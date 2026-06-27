2f107e00384f6f697bd166bcb4eba41ee893532c
Yangcheng Wu <112789642+Scottwyc@users.noreply.github.com>
2026-06-27 11:27:54 +0800

    Update Apps Script web app URL

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.UPLOAD_PORTAL_CONFIG = {
   // Replace this with the deployed Google Apps Script Web App URL.
   // Example: https://script.google.com/macros/s/AKfycbx.../exec
-  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbyVAcJZKv-wesuIqbSS_YTjBOpj93EpdPWH6rBli61d71wCw0da1Myl5ahgCX6TBo9R/exec'
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbyZ-rjKb1vjwvXTrXCrUwK2iFx0rw6u-B5RreQr-i1_-B86G1QUsnTKlM6p-O0ZWGA7/exec'
 };
