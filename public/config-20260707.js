d7e646d557ac525fa4a0822a039227d88ec11352
Yangcheng Wu <112789642+Scottwyc@users.noreply.github.com>
2026-07-07 00:28:43 +0800

    Use versioned frontend asset files

---
+++ public/config-20260707.js
@@ -0,0 +1,6 @@
+window.UPLOAD_PORTAL_CONFIG = {
+  // Replace this with the deployed Google Apps Script Web App URL.
+  // Example: https://script.google.com/macros/s/AKfycbx.../exec
+  // Current deployment is for the sign language recording submission form.
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbye8zXvdBa7quky_hRriRkSStHp2pMUVmys1B2Vw5OGMjuexL3nUKsUs56SE5X8R49x/exec'
+};
